f9d3e9b97e96136b8b62bdc9d161c3934490996f
lavo <lavo@kth.se>
2023-07-19 16:02:33 -0400

    P1 fixed but still large runtime

--- HW2/P1.py
+++ HW2/P1.py
@@ -41,18 +41,31 @@ n, d = X_train.shape
 
 import numpy as np
 
+# This has been changed to utilise matrix multiplication, since my PC wanted to unalive itself
 def train_perceptron(X_train, y_train, T):
     w = np.zeros(d)
-    
+
     for t in range(T):
-        # pick example (x_i,y_i) such that i = arg min_j (y_j * w_t-1 * x_j)
-        i = np.argmin(y_train * np.dot(w, X_train.T))
+        # 1. Compute dot products for all data points at once
+        predictions = np.dot(X_train, w)
+        
+        # 2. Compute the indices of all misclassified data points
+        misclassified_indices = np.where(y_train * predictions <= 0)[0]
+        
+        if len(misclassified_indices) == 0:
+            # No misclassified samples, we've achieved linear separability
+            break
+        
+        # Use one misclassified example to update the weight
+        i = misclassified_indices[0]
         x_i = X_train[i]
         y_i = y_train[i]
+        
+        # Update weights using the misclassified example
+        w += y_i * x_i
 
-        if y_i * np.dot(w, x_i) <= 0:
-            w = w + y_i * x_i
     return w
+
 
 # Create a weight matrix to store weight vectors for each class
 num_classes = 10
@@ -62,12 +75,17 @@ weights = np.zeros((num_classes, X_train.shape[1]))
 for c in range(num_classes):
     y_train_c = np.where(y_train == c, 1, -1)
     weights[c] = train_perceptron(X_train, y_train_c, n*5)
+    print(f"Trained perceptron for class {c}")
 
-def predict(x):
-    scores = np.dot(weights, x)
-    return np.argmax(scores)
+def predict_all(X_test):
+    # Compute scores for each data point for all classes at once
+    scores = np.dot(X_test, weights.T)
+    # Return classes with the highest scores
+    return np.argmax(scores, axis=1)
 
 # Evaluate on test set
-predictions = np.array([predict(x) for x in X_test])
+predictions = predict_all(X_test)
 accuracy = np.mean(predictions == y_test)
 print(f"Accuracy: {accuracy*100:.2f}%")
+
+# NOTE: Running this code takes 5-10 minutes on my PC
