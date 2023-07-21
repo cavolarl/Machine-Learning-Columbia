c1da835a3685e42ab721df20e4b657c8d4cd77df
lavo <lavo@kth.se>
2023-07-20 22:23:40 -0400

    Kernel done but with optimisation issues

--- HW2/KP.py
+++ HW2/KP.py
@@ -11,9 +11,7 @@ data = sp.io.loadmat('digits.mat')
 X = data['X'] / 255   # Normalize the features
 y = data['Y'].flatten()
 
-# Split data, also randomizes the data
 def split_data(X, y, split_ratio):
-    # Generate a permutation of the indices
     indices = np.random.permutation(X.shape[0])
     split_index = int(split_ratio * X.shape[0])
     train_indices, test_indices = indices[:split_index], indices[split_index:]
@@ -21,14 +19,63 @@ def split_data(X, y, split_ratio):
     X_test, y_test = X[test_indices], y[test_indices]
     return X_train, y_train, X_test, y_test
 
+# Split the data
+X_train, y_train, X_test, y_test = split_data(X, y, 0.5)
 
-# Kernel Perceptron
-
-#Algorithm:
-#Initialize = 0
-#For t = 1,2,3,…, T
-#If exists s.t.
-#implicitly working in
-#non-linear kernel space!
+def polynomial_kernel(x_i, x_j, d=10, c=2):
+    return (c + np.dot(x_i, x_j)) ** d
 
 
+# Precompute the kernel matrix
+def compute_kernel_matrix(X, d=10, c=2):
+    K = np.dot(X, X.T)
+    K = (c + K) ** d
+    return K
+
+K_train = compute_kernel_matrix(X_train)
+
+def train_kernel_perceptron(K_train, y_train, T):
+    n_samples = K_train.shape[0]
+    alpha = np.zeros(n_samples)
+    
+    for t in range(T):
+        i = t % n_samples
+        # This is the misclassification condition
+        f_xi = np.sign(np.dot(alpha * y_train, K_train[:, i]))
+        if f_xi != y_train[i]:
+            alpha[i] += 1
+            
+    return alpha
+
+def predict_kernel_perceptron(K_test_point, alphas, y_train):
+    scores = np.zeros(num_classes)
+    for c in range(num_classes):
+        score = np.sum(alphas[c] * y_train * K_test_point)
+        scores[c] = score
+    return np.argmax(scores)
+
+
+num_classes = 10
+alphas = np.zeros((num_classes, X_train.shape[0]))
+
+# Train a kernel perceptron for each class
+for c in range(num_classes):
+    y_train_c = np.where(y_train == c, 1, -1)
+    alphas[c] = train_kernel_perceptron(K_train, y_train_c, len(y_train)*20)
+    print(f"Trained kernel perceptron for class {c}")
+
+# Evaluate on test set
+predictions = []
+for x in X_test:
+    K_test_point = polynomial_kernel(X_train, x)
+    predictions.append(predict_kernel_perceptron(K_test_point, alphas, y_train))
+predictions = np.array(predictions)
+
+# print the distribution of the predictions
+print(np.bincount(predictions))
+print(np.bincount(y_test))
+
+
+
+accuracy = np.mean(predictions == y_test)
+print(f"Accuracy: {accuracy*100:.2f}%")
