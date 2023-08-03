0cced7f8506287b5a18834195889310e91ab114d
lavo <lavo@kth.se>
2023-08-03 14:38:29 -0400

    state

---
+++ HW3/Gaussian.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Number of samples
+n_samples = 10000
+
+# Dimensions to test
+dimensions = [1, 2, 3, 5, 10, 50, 100]
+
+# Create a plot for each dimension
+plt.figure(figsize=(12, 8))
+
+for d in dimensions:
+    # Sample from a d-dimensional Gaussian with mean 0 and identity covariance
+    samples = np.random.randn(n_samples, d)
+    
+    # Compute the squared length of each sample
+    squared_lengths = np.sum(samples**2, axis=1)
+    
+    # Plot histogram of squared lengths
+    plt.hist(squared_lengths, bins=50, alpha=0.5, label=f'd={d}', density=True)
+
+plt.title("Histogram of Squared Lengths of Samples from Gaussian Distributions")
+plt.xlabel("Squared Length")
+plt.ylabel("Density")
+plt.legend()
+plt.show()
